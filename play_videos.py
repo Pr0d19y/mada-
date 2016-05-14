90d9f95b0b7d45b19a5456b0737996183a29da75
Pr0d19y <adi.szes@gmail.com>
2016-05-14 16:32:58 +0000

    new file:   classes/omxplayer.py 	modified:   play_videos.py

--- play_videos.py
+++ play_videos.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import sys
 import os
 import subprocess
+from classes.omxplayer import OMXDriver
 
 GPIO.setmode(GPIO.BCM)
 
@@ -20,27 +21,32 @@ input_state2 = True
 quit_video = True
 player = False
 
-while True:
-            
-	#Read states of inputs
+while True:  
+	#Read states of inputs  
 	input_state1 = GPIO.input(17)
+	omx = OMXDriver(1)
 	input_state2 = GPIO.input(18)
+	#print input_state2,last_state2
 	quit_video = GPIO.input(24)
-
 	#If GPIO(17) is shorted to ground
 	if input_state1 != last_state1:
 		if (player and not input_state1):
 			os.system('killall omxplayer.bin')
-			omxc = subprocess.Popen(['omxplayer', '-b', movie1], stdin=subprocess.PIPE)
+			#omxc = subprocess.Popen(['omxplayer', '-b', movie1], stdin=subprocess.PIPE)
+			omx.play(movie1,"")
 			player = True
 		elif not input_state1:
-			omxc = subprocess.Popen(['omxplayer', '-b', movie1], stdin=subprocess.PIPE)
+			#omxc = subprocess.Popen(['omxplayer', '-b', movie1], stdin=subprocess.PIPE)
+			omx.play(movie1,"")
 			player = True
 	elif input_state2 != last_state2:
 		if player and not input_state2:
-			omxc.stdin.write('q')
+			#omxc.stdin.write('q')
+			print "stopping"
+			print omx.is_running
+			omx.pause
 			player = False
-    	
+	
 	if quit_video == False:
 		os.system('killall omxplayer.bin')
 		player = False
