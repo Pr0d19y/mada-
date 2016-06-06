7b33846078c1a1c0e7534f7e15db05e725fc9624
Pr0d19y <adi.szes@gmail.com>
2016-06-06 08:12:56 +0300

    Added play, pause, remove the parsing which takes too long

--- classes/omxplayer.py
+++ classes/omxplayer.py
@@ -33,16 +33,25 @@ class OMXPlayer(object):
         #(self.audio['streams'], self.video['streams'],
         # self.chapters, self.subtitles) = [int(x) for x in file_props]
         # Get video properties
-        #video_props = self._VIDEOPROP_REXP.match(self._process.readline()).groups()
-        #self.video['decoder'] = video_props[0]
-        #self.video['dimensions'] = tuple(int(x) for x in video_props[1:3])
-        #self.video['profile'] = int(video_props[3])
-        #self.video['fps'] = float(video_props[4])
+        #video_check = self._VIDEOPROP_REXP.match(self._process.readline())
+        #if video_check:
+        #    video_props = video_check.groups()
+        #    self.video['decoder'] = video_props[0]
+        #    self.video['dimensions'] = tuple(int(x) for x in video_props[1:3])
+        #    self.video['profile'] = int(video_props[3])
+        #    self.video['fps'] = float(video_props[4])
+        #else:
+        #    print "Error parsing video properties"
         # Get audio properties
-        #audio_props = self._AUDIOPROP_REXP.match(self._process.readline()).groups()
-        #self.audio['decoder'] = audio_props[0]
-        #(self.audio['channels'], self.audio['rate'],
-        # self.audio['bps']) = [int(x) for x in audio_props[1:]]
+        #audio_check = self._AUDIOPROP_REXP.match(self._process.readline())
+        #if audio_check:
+        #    audio_props = audio_check.groups()
+        #    self.audio['decoder'] = audio_props[0]
+        #    (self.audio['channels'], self.audio['rate'],
+        #     self.audio['bps']) = [int(x) for x in audio_props[1:]]
+        #else:
+        #    print "Error parsing audio properties"
+
 
         #if self.audio['streams'] > 0:
         #    self.current_audio_stream = 1
@@ -51,9 +60,8 @@ class OMXPlayer(object):
         self._position_thread = Thread(target=self._get_position)
         self._position_thread.start()
 
-        if not start_playback:
-            self.toggle_pause()
-        self.toggle_subtitles()
+        #if not start_playback:
+        #    self.toggle_pause()
 
 
     def _get_position(self):
@@ -71,10 +79,15 @@ class OMXPlayer(object):
     def toggle_pause(self):
         if self._process.send(self._PAUSE_CMD):
             self.paused = not self.paused
+            
+    def play(self):
+        if self.paused:
+            self.toggle_pause()
 
-    def toggle_subtitles(self):
-        if self._process.send(self._TOGGLE_SUB_CMD):
-            self.subtitles_visible = not self.subtitles_visible
+    def pause(self):
+        if not self.paused:
+            self.toggle_pause()
+
     def stop(self):
         self._process.send(self._QUIT_CMD)
         self._process.terminate(force=True)
