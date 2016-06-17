999153bee1edba5fe9e9d0f29f4a3569251846f2
orlevi <orlevi1234@gmail.com>
2016-06-17 11:33:38 +0000

    pin polling instead of callback

--- advisor/advisor.py
+++ advisor/advisor.py
@@ -1,11 +1,10 @@
-__author__ = 'Or Levi'
+bg__author__ = 'Or Levi'
 
-#import movie_player
 import advisor_config as config
 import time
 import threading
 import RPi.GPIO as GPIO
-
+from classes.omxplayer import OMXPlayer
 
 class advisor(object):
     """
@@ -20,55 +19,82 @@ class advisor(object):
 
         self.control_gpio = gpio_number
         self.movie = movie
+        self.current_state = None
+        self.polling_tries = 0
         self.last_event_time = 0
-        self.last_event_was_rise = True
-        
+        self.last_event_was_play = True
+
+        self.movie_controller = OMXPlayer(mediafile=self.movie,args="--win 0,0,400,400")
+
         GPIO.setmode(GPIO.BOARD)  # use board numbers (ie pin1, pin2 of board and not of the chip)
-        GPIO.setup(self.control_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.control_gpio, GPIO.BOTH, callback=self.event, bouncetime=50)
+        GPIO.setup(self.control_gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #GPIO.add_event_detect(self.control_gpio, GPIO.BOTH, callback=self.event, bouncetime=100)
+        self.running = True
 
-    def event(self, channel):
+        self.pin_poll_thread = threading.Thread(target=self.poll_input)
+        self.pin_poll_thread.start()
+        
+
+    def event(self):
         self.last_event_time = time.time()
-        current_state = GPIO.input(self.control_gpio)
-        if current_state:            
-            print 'debug print : PLAY THREAD started'
-            self.last_event_was_rise = True
-            play_thread = threading.Thread(target=self.play_after_delay)
-            play_thread.start() 
-        else:  
-            print 'debug print : STOP THREAD started'
-            self.last_event_was_rise = False
+        if self.current_state:
+            print "debug print : play_thread"
+            self.last_event_was_play = False
             stop_thread = threading.Thread(target=self.pause_wait_and_stop)
-            stop_thread.start()             
+            stop_thread.start() 
+        else:  
+            print "debug print : stop_thread"
+            self.last_event_was_play = True
+            play_thread = threading.Thread(target=self.play_after_delay)
+            play_thread.start()             
 
+    def poll_input(self):
+        while self.running:
+            current_state = GPIO.input(self.control_gpio)
+            if current_state != self.current_state and self.polling_tries == config.POLLING_TRIES:  #state has changed and reached stable value
+                self.current_state = current_state
+                self.event()
+            elif current_state != self.current_state and self.polling_tries < config.POLLING_TRIES: #state has changed but we are still waiting for input to stabilize
+                self.polling_tries = self.polling_tries + 1
+            else:                                                                                   #stete has not changed, reset the stability count
+                self.polling_tries = 1                
+            time.sleep(config.POLLING_DELAY)
+            
     def play_after_delay(self):
         ''' this method waits the DELAY_TIME specified in the config file, and then play the video ''' 
         time.sleep(config.DELAY_TIME)
         time_since_last_event = time.time() - self.last_event_time
         # make sure there wasn't another event since this method was called before executing any other commands
-        if time_since_last_event >= config.DELAY_TIME and self.last_event_was_rise:
+        if time_since_last_event >= config.DELAY_TIME and self.last_event_was_play:
             print "debug print : movie PLAY called"
-            #self.movie_controller.play()
+            self.movie_controller.play()
               
     def pause_wait_and_stop(self):
         ''' this method pauses the video, waits the PAUSE_TIME specified in the config file, and then
             stops the video '''
         print "debug print : movie PAUSE called"
-        #self.movie_controller.pause()
+        self.movie_controller.pause()
         time.sleep(config.PAUSE_TIME)
         time_since_last_event = time.time() - self.last_event_time
         # make sure there wasn't another event since this method was called before executing any other commands
-        if time_since_last_event >= config.PAUSE_TIME and not self.last_event_was_rise:
+        if time_since_last_event >= config.PAUSE_TIME and not self.last_event_was_play:
             print "debug print : movie STOP called"
+            self.movie_controller.restart()
             #self.movie_controller.stop()
+            #self.movie_controller = OMXPlayer(mediafile=self.movie,args="--win 0,0,400,400")
+            
 
     def quit(self):
         GPIO.cleanup()
+        self.running = False
+        time.sleep(3)
+        print "debug print: Quitting"
         # TODO: threading cleanup needed?
 
 if __name__ == '__main__':
-    movie = r'path/to/movie'
-    gpio = 37
+    #movie = r'/home/pi/Downloads/eyal_kimchi_4.mpg'
+    movie = r'/home/pi/Downloads/tomato_blink_sequence.mp4'
+    gpio = 32
     player = advisor(gpio_number=gpio, movie=movie)
 
     q = raw_input("Do you want to exit? (Y)")
--- advisor/advisor_config.py
+++ advisor/advisor_config.py
@@ -1,3 +1,6 @@
 # Time constants (all times are in seconds):
 PAUSE_TIME = 2 # if the advisor GPIO input pin is low for shorter than this time, the advisor will continue from the same place
 DELAY_TIME = 1 # the time by which the video play will be delayed after the GPIO input pin rising edge
+
+POLLING_DELAY = 0.005 # time between gpio input polling tries
+POLLING_TRIES = 20    # number of tries with the same gpio value after change to consider it stable (debouncing)
