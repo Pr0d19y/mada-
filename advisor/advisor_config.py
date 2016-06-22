d1adbf3fad1f2ac7c6f36094ee15e00276306ba5
orlevi <orlevi1234@gmail.com>
2016-06-22 19:13:20 +0000

    looping, stopping only according to gpio, movies paths added

--- advisor/advisor_config.py
+++ advisor/advisor_config.py
@@ -1,6 +1,16 @@
+#--------------------------------------------
 # Time constants (all times are in seconds):
+#--------------------------------------------
 PAUSE_TIME = 2 # if the advisor GPIO input pin is low for shorter than this time, the advisor will continue from the same place
 DELAY_TIME = 1 # the time by which the video play will be delayed after the GPIO input pin rising edge
 
 POLLING_DELAY = 0.005 # time between gpio input polling tries
 POLLING_TRIES = 20    # number of tries with the same gpio value after change to consider it stable (debouncing)
+
+#---------------------------------------------------
+#select movie - leave exactly one line un-commented:
+#---------------------------------------------------
+movie = r'/home/pi/Downloads/eyal_kimchi.mp4'
+#movie = r'/home/pi/Downloads/uri_ariel.mp4'
+#movie = r'/home/pi/Downloads/market_intreviews.mp4'
+#movie = r'/home/pi/Downloads/dan_levanon.mp4'
