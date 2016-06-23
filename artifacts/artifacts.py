caa8f52879a5d25a1faa73a59bc369327d4689c3
orlevi <orlevi1234@gmail.com>
2016-06-23 22:37:45 +0000

    output pin, quit pin, config file, etc

--- artifacts/artifacts.py
+++ artifacts/artifacts.py
@@ -1,12 +1,15 @@
+
+
 __author__ = 'okapach'
 
 from classes import omxplayer
 import RPi.GPIO as GPIO
+import artifacts_config as config
 
 
 class artifact(object):
     """
-    play a movie once and turn on a led when a button is pressed
+    play a movie in a loop turn on a led when a button is pressed
     """
 
     def __init__(self, gpio_number, movie):
@@ -15,34 +18,32 @@ class artifact(object):
         print 'GPIO: {}'.format(gpio_number)
 
         self.control_gpio = gpio_number
+        self.led_gpio = 38
+        self.quit_gpio = 40
         self.movie = movie
 
         GPIO.setmode(GPIO.BOARD)  # use board numbers (ie pin1, pin2 of board and not of the chip)
         GPIO.setup(self.control_gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.led_gpio, GPIO.OUT)
+        GPIO.output(self.led_gpio, 0)
         GPIO.add_event_detect(self.control_gpio, GPIO.FALLING, callback=self.event, bouncetime=50)
+        GPIO.add_event_detect(self.quit_gpio, GPIO.FALLING, callback=self.quit, bouncetime=50)
 
-        self.movie_controller = omxplayer.OMXPlayer(mediafile=self.movie, start_playback=True)
+
+        self.movie_controller = omxplayer.OMXPlayer(mediafile=self.movie, start_playback=True, args= '--loop --no-osd')
 
 
     def event(self, channel):
-        current_state = GPIO.input(self.control_gpio)
-        self.movie_controller.stop()
-        self.movie_controller = omxplayer.OMXPlayer(mediafile=self.movie, start_playback=True)
-        print 'event detected, ch: {}, FALLING event'.format(channel)
-        #self.movie_controller.toggle_pause()
-        #self.movie_2_controller.toggle_pause()
+        GPIO.output(self.led_gpio, 1)
+        time.sleep(config.LED_TIME)
+        GPIO.output(self.led_gpio, 0)
 
-    def quit(self):
+    def quit(self, channel):
         GPIO.cleanup()
         self.movie_controller.stop()
 
 
 if __name__ == '__main__':
-    movie = r'/home/pi/Downloads/tzaleket.mp4'
+    movie = config.movie
     gpio = 32
     player = artifact(gpio_number=gpio,  movie=movie)
-
-    q = raw_input("Do you want to exit? (Y)")
-    if q is 'Y':
-        player.quit()
-
