fbd46c4db4f894f2180920d640bcffce1f7c874e
omercoh <omercoh@gmail.com>
2016-06-29 17:00:52 +0000

    Zuccini V1 from exhibition

--- pollunation/code/zuccini.py
+++ pollunation/code/zuccini.py
@@ -12,8 +12,8 @@ debug = not input(debug_jmp)
 # Set Video Files
 idle_video_file          = '/home/pi/mada-/pollunation/videos/zucchini_idle.mp4'
 dust_complete_video_file = '--orientation 180 /home/pi/mada-/pollunation/videos/zucchini_00.mp4'
-idle_video          = OMXPlayer(idle_video_file, loop = True, debug=debug)
-dust_complete_video = OMXPlayer(dust_complete_video_file, debug=debug)
+#idle_video          = OMXPlayer(idle_video_file, loop = True, debug=debug)
+dust_complete_video = OMXPlayer(dust_complete_video_file, debug=debug, loop=True)
 #idle_video = dust_complete_video
 
 # Set GPIO names
@@ -23,19 +23,34 @@ setwarnings(False)
 setup(complete, IN, pull_up_down=PUD_DOWN)
 
 def state_idle():
-    #idle_video.restart()
-    #time.sleep(0.4)
-    idle_video.play()
-    #time.sleep(0.7)
-    #idle_video.pause()
+    # Opening day workaround
     while True:
-        if input(complete):
-            return(state_dust_complete)
+        time.sleep(5e-3)
+        dust_complete_video.play()
+
+    #idle_video.restart()
+    time.sleep(0.4)
+    
+    #idle_video.play()
+    time.sleep(0.7)
+    dust_complete_video.pause()
+    start_time = time.time()
+    while time.time()-start_time < 1:
+        time.sleep(5e-3)
+        if not input(complete):
+            start_time = time.time()
+#        else:
+#            import pdb
+#            pdb.set_trace()
+    return(state_dust_complete)
 
 def state_dust_complete():
     dust_complete_video.play()
     while not dust_complete_video.paused:
+        time.sleep(5e-3)
         pass
     #time.sleep(2) #remove
+#    dust_complete_video.restart()
+#    dust_complete_video.pause()
     return state_idle
   
