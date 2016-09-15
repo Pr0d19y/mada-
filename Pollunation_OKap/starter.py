46cdbe417addac2c5111d946c3b08aefb23bd745
Omer Kapach <omerkap@gmail.com>
2016-09-15 21:58:57 +0000

    [Omer K.] static ip according to identity

--- Pollunation_OKap/starter.py
+++ Pollunation_OKap/starter.py
@@ -1,4 +1,7 @@
+import sys
 import RPi.GPIO as GPIO
+sys.path.append('/home/pi/mada-/classes/')
+import ip_reservations
 GPIO.setmode(GPIO.BOARD)
 
 # Const
@@ -21,15 +24,19 @@ GPIO.setup(zuccini_female_jmp,   GPIO.IN, pull_up_down=GPIO.PUD_UP)
 if not GPIO.input(zuccini_male_jmp):
     print "I'm male Zuccini!"
     from zuccini_male import *
+    ip_reservations.set_static_ip(ip_reservations.IPS['pollination_zuc_male'])
 elif not GPIO.input(zuccini_female_jmp):
     print "I'm female Zuccini!"
     from zuccini_female import *
+    ip_reservations.set_static_ip(ip_reservations.IPS['pollination_zuc_female'])
 elif not GPIO.input(tomato_jmp):
     print "I'm Tomato!"
     from tomato import *
+    ip_reservations.set_static_ip(ip_reservations.IPS['pollination_tomato'])
 elif not GPIO.input(zuccini_fruit_jmp):
     print "I'm Zuccini fruit!"
     from zuccini_fruit import *
+    ip_reservations.set_static_ip(ip_reservations.IPS['pollination_zuc_fruit'])
 
 #def runFSM(state):
 #    while True:
